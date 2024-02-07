c2d167bb30d646939d97908700e66c3f611a9fb2
Daniel Pichardo <danielpichardo716@gmail.com>
2024-02-07 16:43:21 -0500

    Add project files.

---
+++ Shortly.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Shortly.Client.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
