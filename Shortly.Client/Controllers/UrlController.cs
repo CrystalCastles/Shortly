3d2ff738eed534d7cebc4a342dc6c6ccdba48c8d
Daniel Pichardo <danielpichardo716@gmail.com>
2024-02-12 11:25:13 -0500

    Create URL Controller and Model

---
+++ Shortly.Client/Controllers/UrlController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shortly.Client.Controllers {
+    public class UrlController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
